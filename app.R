2ac6b2be75199110d6c00b7044da47509c4c3dd5
claps-hands-in-the-r <mama.elegoet@gmail.com>
2022-12-19 15:16:25 +0100

    ben oui

--- app.R
+++ app.R
@@ -15,20 +15,31 @@ dat<-drop_na(dat)
 
 ui <- fluidPage(
     sidebarLayout(
-        sidebarPanel(
-    # input for page 1 tab1 and page 1 tab 2
-    sliderInput(inputId = "Ideochoice", label = "Select Five Point Ideology (1=Very liberal, 5=Very conservative)",
-                min = 1, max = 5, value = 3)
+        sidebarPanel(    
+    # input and diplay for page 1 tab1 and page 1 tab 2
+            sliderInput(
+                inputId = "Ideo_choice",
+                label = "Select Five Point Ideology (1=Very liberal, 5=Very conservative)",
+                min = 1,
+                max = 5,
+                value = 3
+            )
         ),
-    
-    
-    tabsetPanel(
-        tabPanel("Tab1",plotOutput(outputId = "distplot_tab1")),
-        tabPanel("Tab2",plotOutput(outputId = "distplot_tab2"))
-    
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Tab1", plotOutput(outputId = "distplot_tab1")), 
+                tabPanel("Tab2", plotOutput(outputId = "distplot_tab2"))
+            )
         )
+    ),
+    # input and display for page 2
+    sidebarLayout(
+        sidebarPanel(    
+            checkboxGroupInput(inputId = "checked_genders", label="Select Gender",choices=c(1,2),)
+        ),
+        mainPanel(plotOutput(outputId = "plot3")
+                  )
     )
-    
 )
     
     
@@ -37,28 +48,33 @@ ui <- fluidPage(
         
         # making page 1 tab 1 geom_bar pid7
         output$distplot_tab1 <- renderPlot({
-            dat_selec <- filter(dat, ideo5==input$Ideochoice)
+            dat_selec <- filter(dat, ideo5==input$Ideo_choice)
             ggplot(data=dat_selec, aes(x=pid7)) + xlim(0,8) + ylim(0,125) +
-                geom_bar()+labs(x="7 Point Party ID, 1=Very D, 7= Very R")+
-                theme(axis.title.x = element_text(face="bold"))+
-                theme(axis.title.y = element_text(face="bold"))
+                geom_bar()+labs(x="7 Point Party ID, 1=Very D, 7= Very R")
         })
         # making page 1 tab 2 geom_bar trumsupport
             output$distplot_tab2 <- renderPlot({
-                dat_selec <- filter(dat, ideo5==input$Ideochoice)
+                dat_selec <- filter(dat, ideo5==input$Ideo_choice)
                 ggplot(data=dat_selec, aes(x=CC18_308a)) + xlim(0,5) + ylim(0,150) +
-                    geom_bar()+labs(x="Trump Support", y = "Count")+
-                    theme(axis.title.x = element_text(face="bold"))+
-                    theme(axis.title.y = element_text(face="bold"))
+                    geom_bar()+labs(x="Trump Support", y = "Count")
             
         })
+        
+        # building plot 3 
+            output$plot3 <- renderPlot({
+               
+                ggplot(filter(dat, gender %in% input$checked_genders),aes(x=educ,y=pid7)) +
+                    geom_point() +
+                    geom_jitter()+
+                    geom_smooth(method=lm,se=TRUE)
+            })
         
     }      
         #####Hint: when you make the data table on page 3, you may need to adjust the height argument in the dataTableOutput function. Try a value of height=500
         
         
         
-        
+
 
 
 shinyApp(ui = ui,server = server)
